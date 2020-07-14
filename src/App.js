ba2c7c6c9ef1b12b5120d52ae7035a495ef09973
Adnan Razzaq <adnanrazzaq451@gmail.com>
2020-07-14 15:53:33 +0500

    using usereducer

--- src/App.js
+++ src/App.js
@@ -1,35 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Movie from "./components/Movie";
 import Search from "./components/Search";
 
 const url = "http://www.omdbapi.com/?s=superman&apikey=511ade6b";
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null,
+};
+//reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null,
+      };
+    case "SUCCESS": {
+      return {
+        ...state,
+        movies: action.response,
+        loading: false,
+      };
+    }
+    case "ERROR": {
+      return {
+        ...state,
+        errorMessage: action.error,
+        loading: false,
+      };
+    }
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { movies, loading, errorMessage } = state;
+
   /* states */
-  const [loading, setloading] = useState(true);
+  /*  const [loading, setloading] = useState(true);
   const [movies, setmovies] = useState([]);
-  const [error, seterror] = useState(null);
+  const [error, seterror] = useState(null); */
 
   /* getting data from api */
   useEffect(() => {
     const getdata = async function () {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
       return data;
     };
     getdata().then((data) => {
-      setmovies(data.Search);
-      setloading(false);
+      /*  setmovies(data.Search);
+      setloading(false); */
+      dispatch({
+        type: "SUCCESS",
+        response: data.Search,
+      });
     });
   }, []);
   //search
 
   const search = (searchvalue) => {
-    setloading(true);
-    seterror(null);
+    dispatch({
+      type: "REQUEST",
+    });
+    /* setloading(true);
+    seterror(null); */
     const getdata = async function () {
       const response = await fetch(
         `http://www.omdbapi.com/?s=${searchvalue}&apikey=511ade6b`
@@ -39,11 +78,13 @@ function App() {
     };
     getdata().then((data) => {
       if (data.Response === "True") {
-        setmovies(data.Search);
-        setloading(false);
+        dispatch({ type: "SUCCESS", response: data.Search });
+        /* setmovies(data.Search);
+        setloading(false); */
       } else {
-        seterror(data.Error);
-        setloading(false);
+        dispatch({ type: "ERROR", error: data.Error });
+        /* seterror(data.Error);
+        setloading(false); */
       }
     });
   };
@@ -54,10 +95,10 @@ function App() {
         <Search search={search} />
         <p className="App-intro">Sharing a few of our favourite movies</p>
         <div className="movies">
-          {loading && !error ? (
+          {loading && !errorMessage ? (
             <span>loading...</span>
-          ) : error ? (
-            <div className="errorMessage">{error}</div>
+          ) : errorMessage ? (
+            <div className="errorMessage">{errorMessage}</div>
           ) : (
             movies.map((movie, index) => (
               <Movie key={`${index}-${movie.Title}`} movie={movie} />
